44466ef9de5e1a663d667a84590f1f39024b75c2
Akanksha Agarwal <agarwalakanksha027@gmail.com>
2020-11-09 23:24:21 +0530

    Carousel Screen

---
+++ day1/lib/utilities/styles.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+final kTitleStyle = TextStyle(
+  color: Color(0xFF071A3F),
+  fontFamily: 'Sketch',
+  fontSize: 20.0,
+  height: 1.5,
+);
+
+final kSubtitleStyle = TextStyle(
+  color: Color(0xFF142548),
+  fontFamily: "Poppins",
+  fontSize: 15.0,
+  height: 1.2,
+);
+
+final bSubtitleStyle = TextStyle(
+  color: Color(0xFFF5B8D8),
+  fontFamily: "Poppins",
+  fontSize: 18.0,
+  height: 1.2,
+);
+
+final bTitleStyle = TextStyle(
+  color: Color(0xFFF5B8D8),
+  fontFamily: 'Risque',
+  fontSize: 32.0,
+  height: 1.5,
+);
